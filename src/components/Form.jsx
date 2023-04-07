499ee627d857cc43d325c3fd8d42fe644ca48b47
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-04-07 13:04:45 -0300

    Requisito 2

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,41 +1,89 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
+    const { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick } = this.props;
     return (
       <form>
         <label htmlFor="nome">
           Nome
-          <input data-testid="name-input" type="text" id="nome" />
+          <input
+            data-testid="name-input"
+            type="text"
+            id="nome"
+            value={ cardName }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="descricao">
           Descrição
-          <textarea data-testid="description-input" id="descricao" />
+          <textarea
+            data-testid="description-input"
+            id="descricao"
+            value={ cardDescription }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="attr1">
           Inteligência
-          <input data-testid="attr1-input" type="number" id="attr1" />
+          <input
+            data-testid="attr1-input"
+            type="number"
+            id="attr1"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="attr2">
           Ambição
-          <input data-testid="attr2-input" type="number" id="attr2" />
+          <input
+            data-testid="attr2-input"
+            type="number"
+            id="attr2"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="attr3">
           Controle Emocional
-          <input data-testid="attr3-input" type="number" id="attr3" />
+          <input
+            data-testid="attr3-input"
+            type="number"
+            id="attr3"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="img">
-          <input data-testid="image-input" type="text" id="img" />
+          <input
+            data-testid="image-input"
+            type="text"
+            id="img"
+            value={ cardImage }
+            onChange={ onInputChange }
+          />
         </label>
 
         <label htmlFor="raridade">
           Raridade
-          <select data-testid="rare-input">
+          <select data-testid="rare-input" value={ cardRare } onChange={ onInputChange }>
             <option>normal</option>
             <option>raro</option>
             <option>muito raro</option>
@@ -44,14 +92,41 @@ class Form extends React.Component {
 
         <label htmlFor="super-trunfo">
           Super Trunfo
-          <input data-testid="trunfo-input" type="checkbox" id="super-trunfo" />
+          <input
+            data-testid="trunfo-input"
+            type="checkbox"
+            id="super-trunfo"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
+          />
         </label>
 
-        <button data-testid="save-button">Salvar</button>
+        <button
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+        </button>
 
       </form>
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardRare: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+  hasTrunfo: PropTypes.bool,
+  isSaveButtonDisabled: PropTypes.bool,
+  onInputChange: PropTypes.func,
+  onSaveButtonClick: PropTypes.func,
+}.isRequired;
 
 export default Form;
